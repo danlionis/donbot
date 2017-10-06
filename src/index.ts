7db006dda27edcefa1ef81b10cb6e68f15c5c2c4
Dan Lionis <lionis.dan@gmail.com>
2017-10-06 18:29:38 +0200

    simplified

--- src/index.ts
+++ src/index.ts
@@ -6,34 +6,23 @@ import valid from './utils/message-validator';
 
 class Rainbot extends Discord.Client {
 
-  // LOGIN_TOKEN: string;
-  COMMAND_PREFIX: string;
+  BOT_CMD_PREFIX: string;
 
   constructor() {
     super();
 
-    // this.LOGIN_TOKEN = BOT_TOKEN;
-    // this.COMMAND_PREFIX = BOT_CMD_PREFIX;
+    this.BOT_CMD_PREFIX = BOT_CMD_PREFIX
 
-    // this.login(BOT_TOKEN).then(() => {
-    //   console.log(`Bot connected ${this.user.tag}`);
-    // }).catch((error) => {
-    //   console.log(error);
-    // })
+    this.login(BOT_TOKEN).then(() => {
+      console.log(`Bot connected ${this.user.tag}`);
+    }).catch((error) => {
+      console.log(error);
+    })
 
     this.on('message', (message: Discord.Message) => {
       if (!valid(message)) return;
       let command: Command = parseMessage(message);
       console.log(command);
-    })
-  }
-
-  initialize({ BOT_LOGIN_TOKEN }: Bot) {
-    console.log(BOT_LOGIN_TOKEN);
-    this.login(BOT_LOGIN_TOKEN).then((_) => {
-      console.log(`Rainbot connected with user ${this.user.tag}`);
-    }).catch((err) => {
-      console.log(err);
     })
   }
 }
