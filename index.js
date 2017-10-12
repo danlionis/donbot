1cfc30c3cb3c425a71fd5c97414c928720cbdc80
Dan Lionis <lionis.dan@gmail.com>
2017-10-12 20:15:19 +0200

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const { Bot } = require('./dist/Bot.js');
-const botSettings = require('./bot-settings.json')
-
-console.log(botSettings.WATCHTOGETHER_LINK);
-
-let bot = new Bot(botSettings.BOT_LOGIN_TOKEN, botSettings.BOT_CMD_PREFIX, {WATCHTOGETHER_LINK: botSettings.WATCHTOGETHER_LINK});
-
-bot.connect();
