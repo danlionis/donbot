08b0fc6ab3cf803e8a6c8f14479e73ebedaae57c
Dan Lionis <lionis.dan@gmail.com>
2020-05-11 20:42:45 +0200

    add fuzzy finding for channels and commands

--- src/utils/fuzzy_finder.ts
+++ src/utils/fuzzy_finder.ts
@@ -1,39 +1,69 @@
 import * as Discord from "discord.js";
 // import Fuse from "fuse.js";
+import ls from "js-levenshtein";
 import { Command } from "../parser";
 
 export function find_voice_channel(
   channels: Discord.VoiceChannel[],
   query: string
 ): Discord.VoiceChannel | undefined {
-  // TODO: find better fuzzy search library
+  channels.sort((a, b) => {
+    return a.name.localeCompare(b.name);
+  });
+  const channelNames = channels.map((c) => c.name);
 
-  return channels[0];
+  const distances: number[] = channelNames.map((c) => fuzzyScore(c, query));
 
-  // const options: Fuse.FuseOptions<Discord.VoiceChannel> = {
-  //   keys: ["name"]
-  //   // tokenize: true,
-  //   // matchAllTokens: true,
-  // };
-  // const fuse = new Fuse(channels, options);
+  const minDistance = Math.min(...distances);
 
-  // // splitting and joining required for the finder to work, increases correct match rate drastically
-  // const res: Discord.VoiceChannel[] = fuse.search(query.split("").join(" "));
+  const index = distances.indexOf(minDistance);
 
-  // return res ? res[0] : undefined;
+  return channels[index];
 }
 
 export function find_command(cmds: Command[], query: string): Command {
-  return cmds[0];
+  const cmdNames = cmds.map((c) => c.config.name);
 
-  // const options: Fuse.FuseOptions<Command> = {
-  //   keys: ["full_cmd_name"],
-  //   tokenize: true,
-  //   matchAllTokens: true
-  // };
+  const distances: number[] = cmdNames.map((c) => ls(c, query));
 
-  // const fuse = new Fuse(cmds, options);
+  const minDistance = Math.min(...distances);
 
-  // const res: Command[] = fuse.search(query.split("").join(" "));
-  // return res ? res[0] : undefined;
+  const index = distances.indexOf(minDistance);
+
+  return cmds[index];
+}
+
+/**
+ * Basic fuzzyScore algorithm
+ * lower score means better match
+ * best match: 0
+ * worst match: Infinity
+ */
+function fuzzyScore(target: string, query: string): number {
+  target = target.toLowerCase();
+  query = query.toLowerCase();
+  let it = 0;
+  let iq = 0;
+  let score = 0;
+
+  let lastMatch = -1;
+  let matchCount = 0;
+
+  while (iq < query.length) {
+    while (it < target.length) {
+      if (query[iq] === target[it]) {
+        lastMatch = it;
+        matchCount += 1;
+        break;
+      }
+      score += 1;
+      it += 1;
+    }
+    iq += 1;
+    it = lastMatch + 1;
+  }
+
+  score = score / (matchCount * target.length);
+
+  return score;
 }
